60e7da571b0fc4c6aca40260dbfaeb6f86b5239b
Venkata Vinay Vijjapu <102150068+venkatavinayvijjapu@users.noreply.github.com>
2024-08-23 13:35:03 +0530

    Updated flask application

--- flask_app/utils.py
+++ flask_app/utils.py
@@ -8,7 +8,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-
 # Load API keys from environment variables
 SERPER_API_KEY =os.getenv("SERPER_API_KEY")
 OPENAI_API_KEY =os.getenv("OPENAI_API_KEY")
